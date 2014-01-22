b730d77ec11d922a109aaa4aff42c321854c5f89
paranoyang <paranoyang@gmail.com>
2014-01-21 17:03:13 -0800

    config domain settings

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/hcid/"
-http_images_path = "/hcid/images"
+http_path = "/"
+http_images_path = "/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/hcid/fonts"
-css_dir = "public/hcid/stylesheets"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
